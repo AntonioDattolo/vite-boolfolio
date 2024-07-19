e834322b34668498fc31fb599be297bf13047b0c
AntonioDattolo <antonio19794.a@gmail.com>
2024-07-19 12:47:31 +0200

    vue-router  2

--- src/App.vue
+++ src/App.vue
@@ -30,22 +30,18 @@ export default {
 </script>
 
 <template>
-  <AppHeader/> 
-  <h1>Hello App!</h1>
-  <p>
-    <strong>Current route path:</strong> {{ $route.fullPath }}
-  </p>
-  <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
-    <RouterLink to="/about">Go to About</RouterLink>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
   
-  <AppMain/>
   
-  <AppFooter/>
+ <AppHeader></AppHeader>
+
+
+ <router-view></router-view>
+
+
+  
+  <!-- <AppMain/>
+  
+  <AppFooter/> -->
 </template>
 
 <style scoped>
